cfe69165d086a3e380b072de2f2fd305f6eb2f5c
Gentlehag <info@it-design.biz>
2017-02-15 18:09:43 +0100

    more docs

--- src/BE.FluentGuard/ModelValidationExtensions.cs
+++ src/BE.FluentGuard/ModelValidationExtensions.cs
@@ -9,8 +9,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BE.FluentGuard
 {
+    /// <summary>
+    /// Contains methods for validation models annotated with Attribute from <c>System.ComponentModel.Annotations</c>
+    /// </summary>
     public static class ModelValidationExtensions
     {
+
+        /// <summary>
+        /// Validates wether the given model fulfills all of this annotated definitions
+        /// </summary>
+        /// <typeparam name="T">Type of the model that should be validated</typeparam>
+        /// <param name="rule">The rule on which it should be validated</param>
+        /// <returns>A ValidationRule on which assertions can be made.</returns>
+        /// <exception cref="ValidationException">If the model doesn't fullfill its annotations</exception>
         public static ValidationRule<T> ValidModel<T>(this ValidationRule<T> rule) where T : class
         {
             var context = new ValidationContext(rule.Value);
