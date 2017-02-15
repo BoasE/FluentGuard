6a5b653bfb01b89e3ee86d20afdb0fc9deed19d4
Gentlehag <info@it-design.biz>
2017-02-15 20:58:14 +0100

    doc

--- src/BE.FluentGuard/EnumerableValidationExtensions.cs
+++ src/BE.FluentGuard/EnumerableValidationExtensions.cs
@@ -11,8 +11,18 @@ using System.Linq;
 
 namespace BE.FluentGuard
 {
+    /// <summary>
+    /// Offers methods for asserting Enumerable objects
+    /// </summary>
     public static class EnumerableValidationExtensions
     {
+        /// <summary>
+        /// Asserts that the collection has items
+        /// </summary>
+        /// <typeparam name="T">Type of the items in the collection</typeparam>
+        /// <param name="rule">the validation rule</param>
+        /// <exception cref="ArgumentException">If the collection has no items</exception>
+        /// <returns></returns>
         public static ValidationRule<ICollection<T>> HasItems<T>(this ValidationRule<ICollection<T>> rule)
         {
             if (!rule.Value.HasAny())
