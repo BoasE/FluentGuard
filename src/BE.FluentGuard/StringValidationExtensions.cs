6f7ee143ec767043d40d53447213c065b62a2b46
Gentlehag <info@it-design.biz>
2017-02-15 18:06:37 +0100

    more documentation

--- src/BE.FluentGuard/StringValidationExtensions.cs
+++ src/BE.FluentGuard/StringValidationExtensions.cs
@@ -9,8 +9,17 @@ using System;
 
 namespace BE.FluentGuard
 {
+    /// <summary>
+    /// Contains methods for asserting string values
+    /// </summary>
     public static class StringValidationExtensions
     {
+        /// <summary>
+        /// Asserts that the given string is at least as long as the given length.
+        /// </summary>
+        /// <param name="rule">rule on which it should be asserted.</param>
+        /// <param name="length">minimal length of the string.</param>
+        /// <returns>The validation rule.</returns>
         public static ValidationRule<string> MinLength(this ValidationRule<string> rule, int length)
         {
             if (string.IsNullOrWhiteSpace(rule.Value) || rule.Value.Length < length)
@@ -20,6 +29,14 @@ namespace BE.FluentGuard
             return rule;
         }
 
+        /// <summary>
+        /// Checks that the given string is equal to the passed string.
+        /// </summary>
+        /// <param name="rule">rule on which it should be asserted.</param>
+        /// <param name="value">The value the asserted value should have</param>
+        /// <param name="comparison">The comparison behavior</param>
+        /// <returns>The validation rule.</returns>
+        /// /// <exception cref="ArgumentException">If the asserted string isn't equal to the given strign</exception>
         public static ValidationRule<string> Equal(this ValidationRule<string> rule, string value,
             StringComparison comparison)
         {
@@ -30,6 +47,13 @@ namespace BE.FluentGuard
             return rule;
         }
 
+        /// <summary>
+        /// Checks that the given string is not null or empty
+        /// </summary>
+        /// <param name="rule">rule on which it should be asserted.</param>
+        /// <returns>The validation rule.</returns>
+        /// <exception cref="ArgumentNullException">If the asserted string is null</exception>
+        /// <exception cref="ArgumentException">If the asserted string is empty</exception>
         public static ValidationRule<string> NotNullOrEmpty(this ValidationRule<string> rule)
         {
             if (rule.Value == null)
@@ -45,6 +69,13 @@ namespace BE.FluentGuard
             return rule;
         }
 
+        /// <summary>
+        /// Checks that the given string is not null or whitespace
+        /// </summary>
+        /// <param name="rule">rule on which it should be asserted.</param>
+        /// <returns>The validation rule.</returns>
+        /// <exception cref="ArgumentNullException">If the asserted string is null</exception>
+        /// <exception cref="ArgumentException">If the asserted string is empty or whitespaces only</exception>
         public static ValidationRule<string> NotNullOrWhiteSpace(this ValidationRule<string> rule)
         {
             if (rule.Value == null)
