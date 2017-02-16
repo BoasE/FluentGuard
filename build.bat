60ee00135b3f0aaf77d6a0591315b9baed0f6c3a
Gentlehag <info@it-design.biz>
2017-02-16 22:11:20 +0100

    build batch

---
+++ build.bat
@@ -0,0 +1,3 @@
+dotnet build -o .\bin\452 -f net452 .\src\BE.FluentGuard\BE.FluentGuard.csproj 
+dotnet build -o .\bin\461 -f net461 .\src\BE.FluentGuard\BE.FluentGuard.csproj 
+dotnet build -o .\bin\462 -f net462 .\src\BE.FluentGuard\BE.FluentGuard.csproj 
