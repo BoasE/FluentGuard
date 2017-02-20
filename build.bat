08a6f458920b8bd6509dfe07f9f1bb2c70cf0332
Gentlehag <info@it-design.biz>
2017-02-20 11:41:49 +0100

    minor build change

--- build.bat
+++ build.bat
@@ -1,10 +1,5 @@
-dotnet build -o .\bin\452 -f net452 -c Release .\src\BE.FluentGuard\BE.FluentGuard.csproj 
-dotnet build -o .\bin\452 -f net452 -c Release .\src\BE.FluentGuard.NetFxExtensions\BE.FluentGuard.NetFxExtensions.csproj 
+dotnet build -o .\..\..\bin\452 -f net452 -c Release .\src\BE.FluentGuard\BE.FluentGuard.csproj 
+dotnet build -o .\..\..\bin\461 -f net461 -c Release .\src\BE.FluentGuard\BE.FluentGuard.csproj 
+dotnet build -o .\..\..\bin\462 -f net462 -c Release .\src\BE.FluentGuard\BE.FluentGuard.csproj 
 
-dotnet build -o .\bin\461 -f net461 -c Release .\src\BE.FluentGuard\BE.FluentGuard.csproj 
-dotnet build -o .\bin\461 -f net461 -c Release .\src\BE.FluentGuard.NetFxExtensions\BE.FluentGuard.NetFxExtensions.csproj 
-
-dotnet build -o .\bin\462 -f net462 -c Release .\src\BE.FluentGuard\BE.FluentGuard.csproj 
-dotnet build -o .\bin\462 -f net462 -c Release .\src\BE.FluentGuard.NetFxExtensions\BE.FluentGuard.NetFxExtensions.csproj 
-
-dotnet build -o .\bin\netstandard1.4 -f netstandard1.4 -c Release .\src\BE.FluentGuard\BE.FluentGuard.csproj 
+dotnet build -o .\..\..\bin\netstandard1.4 -f netstandard1.4 -c Release .\src\BE.FluentGuard\BE.FluentGuard.csproj 
