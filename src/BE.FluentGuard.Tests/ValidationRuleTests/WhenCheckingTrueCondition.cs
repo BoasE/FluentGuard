4c2eab538fa72757df6af3a6e4f72865e91f8d9d
Gentlehag <info@it-design.biz>
2017-02-21 08:44:36 +0100

    True / false condition

---
+++ src/BE.FluentGuard.Tests/ValidationRuleTests/WhenCheckingTrueCondition.cs
@@ -0,0 +1,31 @@
+﻿// ==========================================================================
+// WhenCheckingTrueCondition.cs
+// 
+// FlowGuard
+// https://github.com/Gentlehag/FlowGuard
+// ==========================================================================
+
+using System;
+using Xunit;
+
+namespace BE.FluentGuard.Tests.ValidationRuleTests
+{
+    public class WhenCheckingTrueCondition : GivenValidationRule
+    {
+        [Fact]
+        public void ItThrowsIfFalse()
+        {
+            var sut = GetSut("a", "para");
+
+            Assert.Throws<ArgumentException>(() => sut.True(val => val.Equals("b")));
+        }
+
+        [Fact]
+        public void SuceedsWhenTrue()
+        {
+            var sut = GetSut("a", "para");
+
+            sut.True(val => val.Equals("a"));
+        }
+    }
+}
